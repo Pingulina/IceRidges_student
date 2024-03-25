754e33800456469720fac80228f691c2e40d91aa
Lina Sapp <lina.sapp@tuhh.de>
2024-03-25 17:14:18 +0100

    minor changes in main

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ import ridge_computations.ridge_statistics as rs
 def main():
     # init_py.initialize_python()
     # d2j.example() # store the .dat files as .json files for the mooring data
-    ewd.extract_ridge_LI_data(overwrite=True)
+
     # rs.ridge_statistics()
     # d2d.example()
+
+    ### uncomment the following line to run the extract_ridge_LI_data.py file
+    # ewd.extract_ridge_LI_data(overwrite=True)
+
+    ### uncomment the following line to run the ridge_statistics.py file
+    rs.ridge_statistics()
     return None
 
 if __name__ == "__main__":
